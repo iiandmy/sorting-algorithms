03c5c4493dfc03a64e4366ee03a2d5507cd38830
iiandmy <frezhgrand@gmail.com>
2023-03-03 23:56:31 +0300

    init commit

---
+++ src/main/java/by/budevich/App.java
@@ -0,0 +1,13 @@
+package by.budevich;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
