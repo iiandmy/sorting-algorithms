7381a15cdc21568ec033c19e7a7c6ef83a6e53f6
iiandmy <frezhgrand@gmail.com>
2023-03-07 23:48:25 +0300

    fix documentation

--- src/main/java/by/budevich/comparator/RepoBasedComparator.java
+++ src/main/java/by/budevich/comparator/RepoBasedComparator.java
@@ -9,12 +9,12 @@ public interface RepoBasedComparator<T> extends Comparator<T> {
      * Comparing rule is: The less the index, the greater the value.<br>
      * In addition to original rules, listed in Comparator's method,
      * Implementator must ensure that the return value, when unknown value provided, equals:<br>
-     * - Positive integer in case, when o1 - known value in repo, o2 - unknown value in repo;<br>
+     * - Positive integer in case, when o1 - unknown value in repo, o2 - known value in repo;<br>
      * - Zero in case, when o1 - unknown value in repo, o2 - unknown value in repo;<br>
-     * - Negative integer or less in case, when o1 - unknown value in repo, o2 - known value in repo;
-     * @return positive integer - when index of o1's value is less than index of o2's value;<br>
+     * - Negative integer or less in case, when o1 - known value in repo, o2 - unknown value in repo;
+     * @return positive integer - when index of o1's value is greater than index of o2's value;<br>
      *         zero - when index of o1's value equals index of o2's value;<br>
-     *         negative integer - when index of o1's value is greater than index of o2's value;
+     *         negative integer - when index of o1's value is less than index of o2's value;
      */
     int compare(T o1, T o2);
 }
